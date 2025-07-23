85e1b18829042f3ad5b8815941d0e2ca66ff7203
UroojFatima1 <uroojfatma1518@gmail.com>
2025-07-24 02:36:32 +0500

    spelling correction

--- src/app/viewfeedback/page.js
+++ src/app/viewfeedback/page.js
@@ -48,7 +48,7 @@ export default function ViewFeedback()
                         href="/feedback"
                         className="bg-indigo-100 text-indigo-700 px-4 py-2 rounded-md font-medium hover:bg-indigo-200 transition"
                     >
-                        Add Feedback
+                        Add Feedbacks
                     </Link>
                 </div>
 
